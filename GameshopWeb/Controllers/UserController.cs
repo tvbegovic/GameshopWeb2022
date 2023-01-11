9b6299e513f53d438064c0ec2bc148dad82571c1
tvbegovic <tvrtkobegovic@gmail.com>
2023-01-11 11:06:36 +0100

    JWT

---
+++ GameshopWeb/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using GameshopWeb.JWT;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Security.Claims;
+
+namespace GameshopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IJwtAuthManager jwtAuthManager;
+
+        public UserController(IConfiguration configuration, IJwtAuthManager jwtAuthManager)
+        {
+            this.configuration = configuration;
+            this.jwtAuthManager = jwtAuthManager;
+        }
+
+        [HttpGet("login")]
+        public LoginResult Login(string email, string password)
+        {
+            using (var conn = new SqlConnection(
+                configuration.GetConnectionString("gameshopConnString")))
+            {
+                var user = conn.QueryFirstOrDefault<User>("SELECT * FROM [User] WHERE email = @email AND password = @password",
+                    new { email, password });
+                if(user != null)
+                {
+                    var claims = new Claim[] { new Claim(ClaimTypes.Email, email) };
+                    var result = jwtAuthManager.GenerateTokens(email, claims, DateTime.Now);
+                    return new LoginResult
+                    {
+                        User = user,
+                        AccessToken = result.AccessToken,
+                        RefreshToken = result.RefreshToken.TokenString
+                    };
+                }
+            }
+            return null;
+        }
+
+    }
+
+    public class LoginResult
+    {
+        public User User { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
